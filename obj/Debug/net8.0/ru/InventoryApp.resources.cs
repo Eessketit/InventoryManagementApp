3a0ae5a2fbdefd011a4e3889323a5854de4aee16
Shohruz <124710827+Eessketit@users.noreply.github.com>
2026-03-31 17:22:06 +0500

    db update

--- obj/Debug/net8.0/ru/InventoryApp.resources.cs
+++ obj/Debug/net8.0/ru/InventoryApp.resources.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryApp")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute(" ")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0176d105e5227aa48f00bdbd1743217cd55c9f67")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0e0a2771cbb3a01ff6e9e37fbd5abf12a368b98")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
